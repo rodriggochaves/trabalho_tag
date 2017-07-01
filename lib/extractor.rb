c30c9f53928224bc146f67a0003899300bf3cb16
rodriggochaves <rodriggo.chaves@gmail.com>
2017-06-30 22:25:49 -0300

    calculando a matriz de afinidade

--- lib/extractor.rb
+++ lib/extractor.rb
@@ -5,43 +5,68 @@ require 'bigdecimal'
 require 'bigdecimal/util'
 
 class Extractor
+  PHI = '0.5'
+  EULER = '2.718281828'
+
   def extract
     options = { col_sep: ';' }
-    path = "user_knowledge_data/micro.data"
+    path = "user_knowledge_data/mini.data"
     @data = []
     CSV.foreach(path, options) do |line|
       @data << line
     end
   end
 
-  def create_affinity_matrix
-    @affinity = self.create_matrix @data.size
-    @data.each_with_index do |line, outer_index|
-      line_sum = 0
-      for i in 0..4
-        for j in 0..@data.size
-          a = numeric_value(line[i])
-          b = numeric_value(@data[outer_index + 1][j])
-          pp "Vou somar #{a.to_digits} e #{b.to_digits}"
+  def init_affinity_matrix
+    @affinity = self.create_matrix 4
+    @data.each do |line|
+      for i in 0..3
+        sum = 0
+        a = line[i]
+        for j in 0..3
+          if !(i == j)
+            b = line[j]
+            cal = difference(value(a), value(b))
+            @affinity[i][j] += cal
+          end
         end
       end
-      # line.each_with_index do |c, index|
-      #   pp "valor de c: #{c}"
-      #   # pp (c.to_i + @data[outer_index][index + 1].to_i)
-      # end
-      # pp line_sum
     end
+
+    pp @affinity
+
+    @affinity.each do |l|
+      for j in 0..3
+        l[j] = gaussian_kernel(l[j])
+      end
+    end
+
+    pp @affinity
   end
 
   def create_matrix size
     matrix = []
     size.times do
-      matrix << Array.new(5, 0)
+      matrix << Array.new(4, 0)
     end
     matrix
   end
 
-  def numeric_value n
+  def value n
     BigDecimal.new(n.gsub(",", "."))
+  end
+
+  def difference a, b
+    return (a - b) ** value('2')
+  end
+
+  def gaussian_kernel a
+    if a.zero?
+      return 0
+    else
+      b = BigDecimal(a).sqrt(10)
+      c = b * (value('-1.0')) / (value('2') * (value(PHI) ** value('2')) ) 
+      return value(EULER) ** c
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -2,4 +2,4 @@ require_relative './lib/extractor'
 
 e = Extractor.new
 e.extract
-e.create_affinity_matrix
+e.init_affinity_matrix
